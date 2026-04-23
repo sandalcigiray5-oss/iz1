64e4651f0e931f20ad98476cdf20f9a61e575778
sandalcigiray5-oss <sandalcigiray5@gmail.com>
2026-04-23 16:04:16 +0300

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -52,6 +52,13 @@ export default {
 <!DOCTYPE html>
 <html lang="tr">
 <head>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MDMSQC3X');</script>
+<!-- End Google Tag Manager -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>İzmir Lastik Yol Yardım | 0544 391 12 44</title>
@@ -99,6 +106,10 @@ export default {
 </head>
 
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MDMSQC3X"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 <div class="bg-fixed"></div>
 <div class="header">
     <div class="title-group">
